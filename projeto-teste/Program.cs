c21e4be8c5d203bb226e2df3018dc8717bd1c6cb
Igor Gabrig <igor_gabrig@hotmail.com>
2018-05-29 21:38:06 -0300

    Acrecentada a pergunta como voce esta hoje?

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -5,6 +5,7 @@ namespace projeto_teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom Dia");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
