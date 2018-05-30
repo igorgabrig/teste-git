f81b1517efc643be87be5728a78a7d3a6e01199c
Igor Gabrig <igor_gabrig@hotmail.com>
2018-05-30 18:30:00 -0300

    Acrescentando Traços

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -3,9 +3,12 @@
 namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("----------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha Um Bom Dia!");
+            Console.WriteLine("----------------------------------");
 
             Console.ReadLine();
         }
