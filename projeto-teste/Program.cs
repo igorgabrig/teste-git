f4c7b09c771ac2c3706027b2e6037fa838c73882
Igor Gabrig <igor_gabrig@hotmail.com>
2018-05-29 21:23:00 -0300

    Acrecentado uma mensagem de Bom Dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,7 +4,8 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            
+            Console.WriteLine("Bom Dia");
+
             Console.ReadLine();
         }
     }
